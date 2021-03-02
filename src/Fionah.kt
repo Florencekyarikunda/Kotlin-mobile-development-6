e75deb9a0b002683b74cda4966fd47d901cf6c8a
Florencekyarikunda <florencekyarikund@gmail.com>
2021-03-02 21:51:47 +0300

    second commit

---
+++ src/Fionah.kt
@@ -0,0 +1,18 @@
+fun main() {
+ fulname()
+ phoneNumber()
+   print(true)
+}
+fun fulname(){
+    var fulname="Gift Lorna"
+    println(fulname)
+}
+fun phoneNumber(){
+    var phoneNumber="07777777234"
+    println(phoneNumber)
+}
+fun citizenship(){
+    var citizenship=("true")
+    println(true)
+
+}
